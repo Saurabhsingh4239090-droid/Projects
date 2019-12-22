a0fb31ac234f076b441759a27347deadcebf45be
Saurabhsingh4239090-droid <57993740+Saurabhsingh4239090-droid@users.noreply.github.com>
2019-12-22 10:55:40 +0530

    Add files via upload

---
+++ LAB1(1).c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main ()
+{
+    int a,b;
+    printf("enter an integer");
+    scanf("%d",&a);
+    b=a%10;
+    printf("the rightmost digit of the integer is %d",b);
+    return 0;
+}
